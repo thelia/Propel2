c104cb9d4e81a02669c4c8ac31e7d343b8a111fe
Baptiste Cabarrou <bcabarrou@openstudio.fr>
2016-06-24 18:16:23 +0200

    [Thelia] Reintroduce legacy TableMap::TYPE_RAW_COLNAME constant
    Compatibility with code written before PR #741 / commit 68aeb69

--- src/Propel/Runtime/Map/TableMap.php
+++ src/Propel/Runtime/Map/TableMap.php
@@ -51,6 +51,13 @@ class TableMap
     const TYPE_COLNAME = 'colName';
 
     /**
+     * column part of the column tableMap name
+     * e.g. 'AUTHOR_ID'
+     * @deprecated Legacy constant, was removed. Do not use.
+     */
+    const TYPE_RAW_COLNAME = 'rawColName';
+
+    /**
      * column fieldname type
      * e.g. 'author_id'
      */
