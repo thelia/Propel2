c16b5d766e2e512e1e95d82e77e104b2bc3fac01
Alexandre Nozière <118798868+anoziere@users.noreply.github.com>
2025-06-09 18:54:16 +0200

    missing namespace

--- src/Propel/Generator/Builder/DataModelBuilder.php
+++ src/Propel/Generator/Builder/DataModelBuilder.php
@@ -21,6 +21,7 @@ use Propel\Generator\Model\Database;
 use Propel\Generator\Model\Inheritance;
 use Propel\Generator\Model\Table;
 use Propel\Generator\Platform\PlatformInterface;
+use Thelia\Core\Propel\Generator\Builder\Om\EventBuilder;
 
 /**
  * This is the base class for any builder class that is using the data model.
